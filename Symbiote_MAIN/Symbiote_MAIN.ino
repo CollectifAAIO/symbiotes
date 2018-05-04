33df364a2eb8fe3ec8b5e8c90e66c2ef674be60f
Madmind <mail@fabienbourlier.fr>
2018-05-04 23:17:21 +0200

    test GitHub 3

--- Symbiote_MAIN/Symbiote_MAIN.ino
+++ Symbiote_MAIN/Symbiote_MAIN.ino
@@ -14,6 +14,8 @@
 /// You should have received a copy of the GNU Lesser Public License
 /// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+//Test Fab GitHub Main
+
 #include "Variables.h" //
 
 void setup() {
