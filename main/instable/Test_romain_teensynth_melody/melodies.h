3b973e9baf6be3db60efc7b39c5b76cf896de898
Romain Bart <barthelemyds@gmail.com>
2018-05-07 21:50:44 +0200

    Déclenchements de mélodies
    Module de déclenchement de mélodies. Les mélodies (notes et rythmes) sont stockées dans des arrays. J'ai ajouté une fonction de randomness sur les notes et les durées mais elle ne marche pas encore très bien.

---
+++ main/instable/Test_romain_teensynth_melody/melodies.h
@@ -0,0 +1,74 @@
+// Fonction playSong(): les mélodies sont stockées ici.
+// Elles sont constituées des notes (NOTE_C3, etc.),
+// et des rythmes (1 pour ronde, 2 pour blanche, 4 pour noire, etc.).
+// Les deux listes doivent avoir le même nombre d'items.
+// Les silences sont notés "0" dans la mélodie.
+
+int melody[] = {
+  NOTE_C3, 0, NOTE_C3, 0,
+  NOTE_C3, 0, NOTE_C3, 0,
+  NOTE_C3, 0, NOTE_C3, 0,
+  NOTE_C5
+};
+
+int tempo[] = {
+  16, 16, 16, 16,
+  16, 16, 16, 16,
+  16, 16, 16, 16,
+  2
+};
+
+
+int mariomelody[] = {
+  NOTE_E7, NOTE_E7, 0, NOTE_E7,
+  0, NOTE_C7, NOTE_E7, 0,
+  NOTE_G7, 0, 0,  0,
+  NOTE_G6, 0, 0, 0,
+ 
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+ 
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0,
+ 
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+ 
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0
+};
+//Mario main them tempo
+int mariotempo[] = {
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+};
